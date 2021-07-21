9d75b21736b9c4dbfed804bc14c2b02d8cde7378
FloUz-ai <83022872+FloUz-ai@users.noreply.github.com>
2021-07-21 09:34:39 +0200

    Realisation contrat creation et interface blockchain

--- contracts/itemsFactory.sol
+++ contracts/itemsFactory.sol
@@ -1,17 +1,80 @@
 // SPDX-License-Identifier: GPL-3.0
-import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 
-pragma solidity 0.7.6;
+pragma solidity 0.8.1;
+
+import "./interfaceMultiverse.sol";
+import "@openzeppelin/openzeppelin-contracts-master/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/openzeppelin-contracts-master/contracts/access/Ownable.sol";
+
 
 /**
  * The itemsFactory contract does this and that...
  */
-contract itemsFactory is ERC721 {
+contract itemsFactory is ERC721, Ownable, interfaceMultiverse {
   
   constructor () ERC721("PowItems","POW"){}
 
-  struct items{
+  string _monument = "Monument";
+  string _material = "Material";
+  string _card = "Card";
+
+ 
+  struct Item{
   	string name;
   	uint composition;
   }
+
+  Item[] public items;
+  mapping (uint => uint) public exemplaire;
+
+  
+  function createItem(string memory _type,string memory _name) public onlyOwner returns (uint) {
+    
+    uint codeFirstPart;
+    uint codeSecondPart;
+    uint codeThirdPart;
+    uint codeComposition;
+    uint tokenId = items.length;
+
+    if ((keccak256 (bytes(_type))) == (keccak256 (bytes(_monument)))) {
+
+      codeFirstPart =  getCodeMonument(_name);
+      codeSecondPart = getCodeMaterial ("paper");
+    } 
+
+    if ((keccak256 (bytes(_type))) == (keccak256 (bytes(_material)))){
+      codeSecondPart = getCodeMaterial(_name);
+    }
+
+    if ((keccak256 (bytes(_type))) == (keccak256 (bytes(_card)))){
+      codeFirstPart =  getCodeMonument(_name);
+    }
+    codeThirdPart = exemplaire[getCodeBaseComposition(codeFirstPart,codeSecondPart)]+1;
+    codeComposition = getCodeComposition(codeFirstPart,codeSecondPart,codeThirdPart);
+
+    exemplaire[getCodeBaseComposition(codeFirstPart,codeSecondPart)] = codeThirdPart;
+    
+    items.push(Item(_name, codeComposition));
+    _safeMint(msg.sender, tokenId);
+
+    return codeComposition;
+    
+  }
+
+  function getCodeMonument(string memory _nameMonument) internal returns (uint){
+    return uint(keccak256(abi.encodePacked(_nameMonument))) % 1000;
+  }
+
+  function getCodeMaterial(string memory _nameMaterial) internal returns (uint){
+    return (uint(keccak256(abi.encodePacked(_nameMaterial))) % 999)+1;
+  }
+
+  function getCodeBaseComposition(uint _codeItem ,uint _codeMaterial) internal returns (uint){
+    return (_codeItem*1000)+_codeMaterial;
+  }
+
+  function getCodeComposition( uint _codeItem, uint _codeMaterial, uint _codeExemplaire) internal returns(uint){
+    return (getCodeBaseComposition(_codeItem,_codeMaterial)*1000) + _codeExemplaire;
+  }
+  
 }
--- test/itemsFactory.test.js
+++ test/itemsFactory.test.js
@@ -1,23 +1,115 @@
 const itemsFactory = artifacts.require("itemsFactory");
 const { expect } = require('chai');
 const { BN } = require('@openzeppelin/test-helpers');
+const time = require("./helpers/time");
+
 
 
 contract("itemsFactory", accounts => {
 
 	const _name = "PowItems";
 	const _symbol = "POW";
+	const _owner = accounts[0];
+	const _player1 = accounts[1];
+	const _player2 = accounts[2];
+	const _player3 = accounts[3];
+	const _item = "Tour eiffel";
+	const _stone = "Pepite or";
+	const _monument = "Monument";
+    const _material = "Material";
+  	const _card = "Card";
 
 	beforeEach(async ()=> {
 		itemsFactoryInstance = await itemsFactory.new();
 	});
 
-	it("has a name", async () => {
+	xit("retourne 1 to validate the connexion", async () =>{
+		let number = new BN(1);
+		expect(await itemsFactoryInstance.get1.call()).to.be.bignumber.equal(number);		
+	});
+
+	it("has a name", async () =>{
 		expect(await itemsFactoryInstance.name()).to.equal(_name);		
 	});
 
-	it("has a symbol", async () => {
+	it("has a symbol", async () =>{
 		expect(await itemsFactoryInstance.symbol()).to.equal(_symbol);
+	});
+	context ("create tour eiffel in paper" , async () => {
+
+		xit("create the item Tour eiffel code 294", async () => {
+
+			let targetedComposition = new BN(294);
+			const valeur = await itemsFactoryInstance.createItem.call(_monument, _item, {from: _owner});
+			expect(valeur).to.be.bignumber.equal(targetedComposition);
+			})
+
+		xit("create the base composition of the item Tour eiffel in paper code 294509", async () => {
+
+			let targetedComposition = new BN(294509);
+			const valeur = await itemsFactoryInstance.createItem.call(_monument, _item,  {from: _owner});
+			expect(valeur).to.be.bignumber.equal(targetedComposition);
+			})
+
+		it("create the first exemplaire item Tour eiffel in paper code 294509001", async () => {
+
+			let targetedComposition = new BN(294509001);
+			const valeur = await itemsFactoryInstance.createItem.call(_monument, _item,  {from: _owner});
+			expect(valeur).to.be.bignumber.equal(targetedComposition);
+			})
+
+		
+		it("create the 2 first exemplaire item Tour eiffel in paper code 294509001 and 294509002" , async () => {
+			
+			let targetedComposition2 = new BN(294509002);
+			await itemsFactoryInstance.createItem(_monument, _item,  {from: _owner});
+			const valeur2 =  await itemsFactoryInstance.createItem.call(_monument, _item,  {from: _owner});
+			expect(valeur2).to.be.bignumber.equal(targetedComposition2);
+			})
+			
+
+	});
+
+	context ("create a gold stone" , async () => {
+
+		it("create the first exemplaire item Pepite or code 000992001", async () => {
+
+			let targetedComposition = new BN(000992001);
+			const valeur = await itemsFactoryInstance.createItem.call(_material, _stone,  {from: _owner});
+			expect(valeur).to.be.bignumber.equal(targetedComposition);
+			})
+
+	});
+
+	context ("create a card" , async () => {
+
+		it("create the first card Tour eiffel code 2940000001", async () => {
+
+			let targetedComposition = new BN(294000001);
+			const valeur = await itemsFactoryInstance.createItem.call(_card, _item,  {from: _owner});
+			expect(valeur).to.be.bignumber.equal(targetedComposition);
+			})
+	});
+
+	it("create an item and attribuate it to the owner", async () =>{
+		let _balance = new BN(1);
+		await itemsFactoryInstance.createItem(_material, _stone,  {from: _owner});
+		expect(await itemsFactoryInstance.balanceOf(_owner)).to.be.bignumber.equal(_balance);
+	});
+
+	it("set a new player", async ()=> {
+
+		await itemsFactoryInstance.setMultiversePlayer( _player1, "albert");
+		expect(await itemsFactoryInstance.multiverseIds.call("albert")).to.equal(_player1);
 	})
+
+	it("update a player", async () => {
+
+		await itemsFactoryInstance.setMultiversePlayer( _player1, "albert");
+		await itemsFactoryInstance.updateMultiversePlayer( "albert", 2, 2);
+		expect(await itemsFactoryInstance.multiverseData.call(_player1)).to.include({login : "albert"});
+	})
+
+
 
 })
